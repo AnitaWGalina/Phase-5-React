711585ace697ea419bb439968dc302430b30e06a
Dennismarangu <dennismutumamarangu@gmail.com>
2023-07-29 23:01:50 +0300

    feat: FarmingGroupAdminServices update

--- phase5-react-app/src/Components/FarmingGroupAdminServices.js
+++ phase5-react-app/src/Components/FarmingGroupAdminServices.js
@@ -1,9 +1,14 @@
+// FarmingGroupAdminServices.js
 import React from 'react';
 
-
 const FarmingGroupAdminServices = () => {
-
+  // Implement forms for services here
+  return (
+    <div>
+      <h1>Services for Farmers</h1>
+      <p>Form for services</p>
+    </div>
+  );
 };
 
 export default FarmingGroupAdminServices;
-
