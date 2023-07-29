268806d559d9bce91d9963fba42b9c7956a0da36
Dennismarangu <dennismutumamarangu@gmail.com>
2023-07-29 23:04:44 +0300

    feat: PublicClientServices update

--- phase5-react-app/src/Components/PublicClientServices.js
+++ phase5-react-app/src/Components/PublicClientServices.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const PublicClientServices = () => {
-
+    return (
+        <div>
+          <h1>Services for Public Clients</h1>
+          <p>Form for rent land</p>
+          <p>Form for lease land</p>
+        </div>
+      );
 };
 
 export default PublicClientServices;
