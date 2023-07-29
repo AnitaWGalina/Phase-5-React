2728dea03a884f80ccfa1f0016b38cedf6891d7f
Dennismarangu <dennismutumamarangu@gmail.com>
2023-07-29 23:03:29 +0300

    feat: PublicClientLandingPage update

--- phase5-react-app/src/Components/PublicClientLandingPage.js
+++ phase5-react-app/src/Components/PublicClientLandingPage.js
@@ -2,7 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const PublicClientLandingPage = () => {
-
+    return (
+        <div>
+          <ul>
+            <li>
+              <Link to="/products">Products</Link>
+            </li>
+            <li>
+              <Link to="/services">Services</Link>
+            </li>
+          </ul>
+        </div>
+      );
 };
 
 export default PublicClientLandingPage;
