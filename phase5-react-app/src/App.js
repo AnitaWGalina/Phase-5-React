1ab773cd520184cccd5c696d426460a8c3ce2ce9
Dennismarangu <dennismutumamarangu@gmail.com>
2023-07-29 22:36:40 +0300

    feat: App.js set-up

--- phase5-react-app/src/App.js
+++ phase5-react-app/src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import FarmingGroupAdminLandingPage from './FarmingGroupAdminLandingPage';
+import PublicClientLandingPage from './PublicClientLandingPage';
 
 function App() {
   return (
-    <div className="App">
-       
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={FarmingGroupAdminLandingPage} />
+        <Route exact path="/public-client" component={PublicClientLandingPage} />
+      </Switch>
+    </Router>
   );
-}
+};
+
 
 export default App;
