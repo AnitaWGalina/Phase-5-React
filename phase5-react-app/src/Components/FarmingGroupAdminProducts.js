2a75fa3cbbf7a7b83be69db9be191aa92de990ed
Dennismarangu <dennismutumamarangu@gmail.com>
2023-07-29 23:26:28 +0300

    feat: FarmingGroupAdminProducts fetch

--- phase5-react-app/src/Components/FarmingGroupAdminProducts.js
+++ phase5-react-app/src/Components/FarmingGroupAdminProducts.js
@@ -1,6 +1,18 @@
 import React, { useEffect, useState } from 'react';
 
 const FarmingGroupAdminProducts = () => {
+ const [products, setProducts] = useState([]);
+
+ useEffect(() => {
+    // Fetch products from the API
+    fetch('/api/products')
+      .then(response => response.json())
+      .then(data => setProducts(data))
+      .catch(error => console.error(error));
+  }, []);
+
+
+     
 
 }
 
