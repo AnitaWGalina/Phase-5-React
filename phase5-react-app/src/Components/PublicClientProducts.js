30d08516b63c60339d6b761d63f2561bc04fa8b6
Dennismarangu <dennismutumamarangu@gmail.com>
2023-07-29 23:34:00 +0300

    feat: PublicClientProducts update

--- phase5-react-app/src/Components/PublicClientProducts.js
+++ phase5-react-app/src/Components/PublicClientProducts.js
@@ -11,9 +11,23 @@ const PublicClientProducts = () => {
       .catch(error => console.error(error));
   }, []);
 
-  
 
-}
+  return (
+    <div>
+      <h1>Farm Utilities and Equipment</h1>
+      <ul>
+        {products.map(product => (
+          <li key={product.id}>
+            <p>{product.ProductName}</p>
+            <p>Price: ${product.ProductPrice}</p>
+            {/* Add the image here using product.ProductImage */}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+
+};
 
 export default PublicClientProducts;
 
