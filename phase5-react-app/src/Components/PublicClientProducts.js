877e1c1d884d97a2743c1d7b26c0237db4b967f5
Dennismarangu <dennismutumamarangu@gmail.com>
2023-07-29 23:31:30 +0300

    feat: PublicClientProducts update

--- phase5-react-app/src/Components/PublicClientProducts.js
+++ phase5-react-app/src/Components/PublicClientProducts.js
@@ -2,6 +2,16 @@ import React, { useEffect, useState } from 'react';
 
 
 const PublicClientProducts = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/products')
+      .then(response => response.json())
+      .then(data => setProducts(data))
+      .catch(error => console.error(error));
+  }, []);
+
+  
 
 }
 
