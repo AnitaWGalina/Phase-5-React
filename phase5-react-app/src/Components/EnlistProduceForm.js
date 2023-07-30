2a26b46bf0f98aa3373609c8b5b827f519baf606
Dennismarangu <dennismutumamarangu@gmail.com>
2023-07-30 09:29:55 +0300

    feat: EnlistProduceForm update

--- phase5-react-app/src/Components/EnlistProduceForm.js
+++ phase5-react-app/src/Components/EnlistProduceForm.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
 
 const EnlistProduceForm = () => {
+  const [productId, setProductId] = useState('');
+  const [productUnitPrice, setProductUnitPrice] = useState('');
+  const [quantity, setQuantity] = useState('');
 
+  
 };
 
 export default EnlistProduceForm;
