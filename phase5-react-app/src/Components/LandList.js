52e7a772abf303d7a6535ce0c369279f1646952a
Dennismarangu <dennismutumamarangu@gmail.com>
2023-07-30 10:59:50 +0300

    feat: LandList fetch

--- phase5-react-app/src/Components/LandList.js
+++ phase5-react-app/src/Components/LandList.js
@@ -1,6 +1,14 @@
 import React, { useState, useEffect } from 'react';
 
 const LandList = () => {
+  const [lands, setLands] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/lands')
+      .then((response) => response.json())
+      .then((data) => setLands(data))
+      .catch((error) => console.error('Error fetching lands:', error));
+  }, []);
 
 };
 
