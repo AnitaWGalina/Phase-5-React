4c7fcf288b53c585bc4af2e00551dbaa6dd3aa5d
Dennismarangu <dennismutumamarangu@gmail.com>
2023-07-30 11:27:26 +0300

    feat: TrainigForm update

--- phase5-react-app/src/Components/TrainingForm.js
+++ phase5-react-app/src/Components/TrainingForm.js
@@ -1,6 +1,16 @@
 import React, { useState } from 'react';
 
 const TrainingForm = () => {
+    const [dateOfTraining, setDateOfTraining] = useState('');
+    const [numberOfTrainees, setNumberOfTrainees] = useState('');
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      const registrationFee = 7000;
+      const totalCost = numberOfTrainees * registrationFee;
+      console.log('Training form submitted!', dateOfTraining, numberOfTrainees, totalCost);
+    };
+
 
 };
 
