198f9b62997b916d75d95aa62ceec8917e1d8a25
Dennismarangu <dennismutumamarangu@gmail.com>
2023-07-30 10:37:54 +0300

    feat: EnlistProduceForm update

--- phase5-react-app/src/Components/EnlistProduceForm.js
+++ phase5-react-app/src/Components/EnlistProduceForm.js
@@ -4,15 +4,16 @@ const EnlistProduceForm = () => {
   const [productId, setProductId] = useState('');
   const [productUnitPrice, setProductUnitPrice] = useState('');
   const [quantity, setQuantity] = useState('');
+  const [produceType, setProduceType] = useState('sellLocally'); // Default to "Sell Locally"
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('Form submitted!', productId, productUnitPrice, quantity);
+    console.log('Form submitted!', productId, productUnitPrice, quantity, produceType);
   };
 
   return (
     <div>
-     <h1>Enlist Produce</h1>
+      <h1>Enlist Produce</h1>
       <form onSubmit={handleSubmit}>
         <label>
           Product ID:
@@ -38,12 +39,17 @@ const EnlistProduceForm = () => {
             onChange={(e) => setQuantity(e.target.value)}
           />
         </label>
+        <label>
+          Produce Type:
+          <select value={produceType} onChange={(e) => setProduceType(e.target.value)}>
+            <option value="sellLocally">Sell Locally</option>
+            <option value="export">Export Produce</option>
+          </select>
+        </label>
         <button type="submit">Submit</button>
       </form>
     </div>
-  )
-
+  );
 };
 
 export default EnlistProduceForm;
-
