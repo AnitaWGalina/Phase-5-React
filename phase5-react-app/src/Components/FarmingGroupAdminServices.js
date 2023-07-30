d3066f65d52a11cccaa4d36746ade374e6de9470
Dennismarangu <dennismutumamarangu@gmail.com>
2023-07-30 10:26:53 +0300

    feat: FarmingGroupAdminServices update

--- phase5-react-app/src/Components/FarmingGroupAdminServices.js
+++ phase5-react-app/src/Components/FarmingGroupAdminServices.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const FarmingGroupAdminServices = () => {
-  // Implement forms for services here
+
   return (
     <div>
       <h1>Services for Farmers</h1>
       <ul>
         <li>
           <Link to="/enlist-produce">Enlist Produce</Link>
+        </li>
+        <li>
+          <Link to="/lease-land">Lease Land</Link> {/* Add the "lease land" link */}
         </li>
         {/* Add other child links for other services as needed */}
       </ul>
