70eccd244e09f096f2ced7ad639e9b02fed2003a
Dennismarangu <dennismutumamarangu@gmail.com>
2023-07-30 10:25:10 +0300

    feat: LeaseLandForm update

--- phase5-react-app/src/Components/LeaseLandForm.js
+++ phase5-react-app/src/Components/LeaseLandForm.js
@@ -2,7 +2,19 @@ import React, { useState } from 'react';
 
 
 const LeaseLandForm = () => {
+    const [landId, setLandId] = useState('');
+    const [typeOfOperation, setTypeOfOperation] = useState('');
+    const [landSize, setLandSize] = useState('');
+    const [operationDuration, setOperationDuration] = useState('');
+    const [operationPrice, setOperationPrice] = useState('');
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Handle form submission logic here, e.g., sending data to the server
+        console.log('Lease land form submitted!', landId, typeOfOperation, landSize, operationDuration, operationPrice);
+      };
+
+      
 };
 
 export default LeaseLandForm;
