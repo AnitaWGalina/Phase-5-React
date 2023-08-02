d41c3b09ac6b2a6171ef569d11598deeed093c9f
MarkChege <mark.wanyoike@strathmore.edu>
2023-08-02 09:21:55 +0300

    Add files 1: Homepage &  Navbar

---
+++ phase5-react-app/src/component/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import 'google-fonts'; // Import the google-fonts package
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <h1 className="text-4xl font-bold mb-1 text-center font-inika-yellow">
+        Where Nature Meets Innovation,
+      </h1>
+      <h1 className="text-4xl font-bold mb-8 text-center font-inika-white">
+        Cultivating a Flourishing Tomorrow
+      </h1>
+    </div>
+  );
+}
+
+export default Home;
