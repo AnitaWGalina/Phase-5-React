ea9509c24b026689de3cd1eff6f1f81afff1ae5f
MarkChege <mark.wanyoike@strathmore.edu>
2023-08-02 13:05:35 +0300

    linked all pages

--- phase5-react-app/src/App.js
+++ phase5-react-app/src/App.js
@@ -1,20 +1,21 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom"; // Import the BrowserRouter component
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Import the BrowserRouter and Routes components
 import Login from "./Login";
 import SignUpForm from "./SignUp";
-import HomePage from './component/HomePage'; // Check the file name and path here
-import NavBar from './component/NavBar';
+import HomePage from "./component/HomePage"; // Check the file name and path here
+import NavBar from "./component/NavBar";
 import './App.css';
-
 
 const App = () => {
   return (
     <div className="App">
       <Router>
-        <SignUpForm />
-        <Login />
         <NavBar />
-        <HomePage />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUpForm />} />
+        </Routes>
       </Router>
     </div>
   );
--- phase5-react-app/src/component/NavBar.js
+++ phase5-react-app/src/component/NavBar.js
@@ -1,6 +1,49 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Navbar = () => {
+const NavBar = () => {
+  // Define the 'styles' object here
+  const styles = {
+    navbar: {
+      backgroundColor: 'black',
+      color: 'white',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      padding: '10px 20px',
+      boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
+      fontFamily: 'Arial, sans-serif',
+      fontSize: '18px',
+    },
+    logo: {
+      display: 'flex',
+      alignItems: 'center',
+    },
+    logoBlack: {
+      fontSize: '24px',
+      fontWeight: 'bold',
+      color: 'white',
+    },
+    logoYellow: {
+      fontSize: '24px',
+      fontWeight: 'bold',
+      color: '#FFD700', /* Yellow color code */
+    },
+    menu: {
+      listStyle: 'none',
+      margin: 0,
+      padding: 0,
+      display: 'flex',
+      alignItems: 'center',
+    },
+    menuItems: {
+      marginLeft: '20px',
+      textDecoration: 'none',
+      color: 'white',
+      transition: '0.2s ease-out',
+    },
+  };
+
   return (
     <nav style={styles.navbar}>
       <div style={styles.logo}>
@@ -8,56 +51,14 @@ const Navbar = () => {
         <span style={styles.logoYellow}>BIX</span>
       </div>
       <ul style={styles.menu}>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Contact Us</a></li>
-        <li><a href="#">Signup</a></li>
-        <li><a href="#">Login</a></li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/contact">Contact Us</Link></li>
+        <li><Link to="/signup">Signup</Link></li>
+        <li><Link to="/login">Login</Link></li>
       </ul>
     </nav>
   );
 };
 
-const styles = {
-  navbar: {
-    backgroundColor: 'black',
-    color: 'white',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    padding: '10px 20px',
-    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
-    fontFamily: 'Arial, sans-serif',
-    fontSize: '18px',
-  },
-  logo: {
-    display: 'flex',
-    alignItems: 'center',
-  },
-  logoBlack: {
-    fontSize: '24px',
-    fontWeight: 'bold',
-    color: 'white',
-  },
-  logoYellow: {
-    fontSize: '24px',
-    fontWeight: 'bold',
-    color: '#FFD700', /* Yellow color code */
-  },
-  menu: {
-    listStyle: 'none',
-    margin: 0,
-    padding: 0,
-    display: 'flex',
-    alignItems: 'center',
-  },
-  menuItems: {
-    marginLeft: '20px',
-    textDecoration: 'none',
-    color: 'white',
-    transition: '0.2s ease-out',
-  },
- 
-};
-
-export default Navbar;
+export default NavBar;
