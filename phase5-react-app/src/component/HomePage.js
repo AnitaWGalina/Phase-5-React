4b2fa9d9de9705c0b88caa50209d87874b557fec
MarkChege <mark.wanyoike@strathmore.edu>
2023-08-02 11:01:34 +0300

    homepage

---
+++ phase5-react-app/src/component/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'google-fonts'; // Import the google-fonts package
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <h1 className="text-4xl font-bold mb-8 text-center font-inika">
+        Where Nature Meets Innovation, Cultivating a Flourishing Tomorrow
+      </h1>
+    </div>
+  );
+}
+
+export default Home;
