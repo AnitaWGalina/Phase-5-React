af62bd298ec104dff7f08ae5d277c4d7c6b9ac4c
MarkChege <mark.wanyoike@strathmore.edu>
2023-08-02 12:52:47 +0300

    login and sign up page added

--- phase5-react-app/src/App.js
+++ phase5-react-app/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom"; // Import the BrowserRouter component
 import Login from "./Login";
 import SignUpForm from "./SignUp";
-import React from 'react';
 import HomePage from './component/HomePage'; // Check the file name and path here
 import NavBar from './component/NavBar';
 import './App.css';
