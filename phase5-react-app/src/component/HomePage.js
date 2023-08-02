d76dbe9f0c1c7e53fa09b296f10ec135c3244258
MarkChege <mark.wanyoike@strathmore.edu>
2023-08-02 13:21:28 +0300

    add products on the homepage and what we offer

--- phase5-react-app/src/component/HomePage.js
+++ phase5-react-app/src/component/HomePage.js
@@ -3,13 +3,34 @@ import 'google-fonts'; // Import the google-fonts package
 
 const Home = () => {
   return (
-    <div className="home-container">
-      <h1 className="text-4xl font-bold mb-1 text-center font-inika-yellow"> {/* Reduce the margin-bottom to mb-1 */}
-        Where Nature Meets Innovation,
-      </h1>
-      <h1 className="text-4xl font-bold mb-8 text-center font-inika-white">
-        Cultivating a Flourishing Tomorrow
-      </h1>
+    <div>
+      <div className="hero-container">
+        <h1 className="hero-title">Where Nature Meets Innovation</h1>
+        <h1 className="hero-subtitle">Cultivating a Flourishing Tomorrow</h1>
+      </div>
+      <div className="product-container">
+        <div className="product-item">
+          <img className="product-image" src="product1.jpg" alt="Product 1" />
+          <h2 className="product-title">Product 1</h2>
+          <p className="product-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <span className="product-price">$19.99</span>
+          <button className="product-button">Add to Cart</button>
+        </div>
+        <div className="product-item">
+          <img className="product-image" src="product2.jpg" alt="Product 2" />
+          <h2 className="product-title">Product 2</h2>
+          <p className="product-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <span className="product-price">$24.99</span>
+          <button className="product-button">Add to Cart</button>
+        </div>
+        <div className="product-item">
+          <img className="product-image" src="product3.jpg" alt="Product 3" />
+          <h2 className="product-title">Product 3</h2>
+          <p className="product-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <span className="product-price">$14.99</span>
+          <button className="product-button">Add to Cart</button>
+        </div>
+      </div>
     </div>
   );
 }
