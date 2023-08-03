158bd6886ef93f43a72a30cc1e71399e798060bf
MarkChege <mark.wanyoike@strathmore.edu>
2023-08-03 15:45:03 +0300

    create a new component for hearder

---
+++ phase5-react-app/src/component/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="hero-container">
+      <h1 className="hero-title">Where Nature Meets Innovation</h1>
+      <h1 className="hero-subtitle">Cultivating a Flourishing Tomorrow</h1>
+    </div>
+  );
+}
+
+export default Header;
