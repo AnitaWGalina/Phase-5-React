c8ccc5e7461881e961f4e96c7c25e8ad9b1dabdf
anitagalina <anitagalina21@gmail.com>
2023-08-03 10:29:47 +0300

    Updated changes

--- phase5-react-app/src/component/Profile.js
+++ phase5-react-app/src/component/Profile.js
@@ -3,7 +3,7 @@ import { useAuth } from "../context/AuthContext";
 import { NavLink } from "react-router-dom";
 
 const Profile = () => {
-  const { user } = useAuth();
+  const { user, logout } = useAuth();
 
   if (!user) {
     return <p>Please log in to view your profile.</p>;
@@ -12,16 +12,16 @@ const Profile = () => {
   return (
     <div>
       <h2>Welcome, {user.name}!</h2>
-        <h1>Status: {user.status}</h1>
-        <h1>Group Size: {user.group_number}</h1>
-        <h1>Email Address: {user.email}</h1>
-        <h1>Phone Number: {user.phone_number}</h1>
-        <h1>Location: {user.location}</h1>
-      {/* <button onClick={logout}>Logout</button> */}
-        <NavLink to="/update_account">
-            <button >Update My Account</button>
-        </NavLink>
-        <button>Delete My Account</button>
+      <h1>Status: {user.status}</h1>
+      <h1>Group Size: {user.group_number}</h1>
+      <h1>Email Address: {user.email}</h1>
+      <h1>Phone Number: {user.phone_number}</h1>
+      <h1>Location: {user.location}</h1>
+      <button onClick={logout}>Logout</button>
+      <NavLink to="/update_account">
+        <button>Update My Account</button>
+      </NavLink>
+      <button>Delete My Account</button>
     </div>
   );
 };
