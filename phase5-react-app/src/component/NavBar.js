51bc18f811369ba68bcbcea6bbe0c19c07ecba7d
MarkChege <mark.wanyoike@strathmore.edu>
2023-08-03 23:38:54 +0300

    changed navbar

--- phase5-react-app/src/component/NavBar.js
+++ phase5-react-app/src/component/NavBar.js
@@ -1,107 +1,96 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+// import HoverBackground from './HoverBackground'; 
 
 const NavBar = () => {
-  const styles = {
-    navbar: {
-      backgroundColor: 'black',
-      color: 'white',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'space-between',
-      padding: '10px 20px',
-      boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
-      fontFamily: 'Arial, sans-serif',
-      fontSize: '18px',
-      transition: 'box-shadow 0.2s', // Add transition for box shadow
-    },
-    navbarHover: {
-      boxShadow: '0 4px 6px rgba(0, 0, 0, 0.3)', // Custom style for hover box shadow
-    },
-    logo: {
-      display: 'flex',
-      alignItems: 'center',
-    },
-    logoBlack: {
-      fontSize: '24px',
-      fontWeight: 'bold',
-      color: 'white',
-    },
-    logoYellow: {
-      fontSize: '24px',
-      fontWeight: 'bold',
-      color: 'yellow',
-    },
-    menu: {
-      listStyle: 'none',
-      margin: 0,
-      padding: 0,
-      display: 'flex',
-      alignItems: 'center',
-    },
-    menuItems: {
-      marginLeft: '20px',
-      textDecoration: 'none',
-      color: 'white',
-      transition: 'color 0.2s',
-    },
-    activeMenuItem: {
-      color: 'green', // Custom style for active menu item when cursor is on top
-    },
-  };
-
-  const handleHover = () => {
-    // Add the hover box shadow when the cursor is on top of the navbar
-    const navbar = document.getElementById('navbar');
-    navbar.style.boxShadow = styles.navbarHover.boxShadow;
-  };
-
-  const handleLeave = () => {
-    // Remove the hover box shadow when the cursor leaves the navbar
-    const navbar = document.getElementById('navbar');
-    navbar.style.boxShadow = styles.navbar.boxShadow;
-  };
-
   return (
-    <nav
-      id="navbar"
-      style={styles.navbar}
-      onMouseEnter={handleHover}
-      onMouseLeave={handleLeave}
-    >
-      <div style={styles.logo}>
-        <span style={styles.logoBlack}>AGRI</span>
-        <span style={styles.logoYellow}>BIX</span>
-      </div>
-      <ul style={styles.menu}>
+    <NavContainer>
+      <LogoContainer>
+        <NavLink to="/">
+          <LogoText>AGRI</LogoText>
+          <LogoTextYellow>BIX</LogoTextYellow>
+        </NavLink>
+      </LogoContainer>
+      <MenuList>
         <li>
-          <NavLink to="/" exact style={styles.menuItems} activeStyle={styles.activeMenuItem}>
+          <StyledNavLink to="/" exact>
             Home
-          </NavLink>
+          </StyledNavLink>
         </li>
         <li>
-          <NavLink to="/about" style={styles.menuItems} activeStyle={styles.activeMenuItem}>
-            About
-          </NavLink>
+          <StyledNavLink to="/about">About</StyledNavLink>
         </li>
         <li>
-          <NavLink to="/contact" style={styles.menuItems} activeStyle={styles.activeMenuItem}>
-            Contact Us
-          </NavLink>
+          <StyledNavLink to="/contact">Contact Us</StyledNavLink>
         </li>
         <li>
-          <NavLink to="/signup" style={styles.menuItems} activeStyle={styles.activeMenuItem}>
-            Signup
-          </NavLink>
+          <StyledNavLink to="/signup">Signup</StyledNavLink>
         </li>
         <li>
-          <NavLink to="/login" style={styles.menuItems} activeStyle={styles.activeMenuItem}>
-            Login
-          </NavLink>
+          <StyledNavLink to="/login">Login</StyledNavLink>
         </li>
-      </ul>
-    </nav>
+      </MenuList>
+    </NavContainer>
   );
 };
+
+const NavContainer = styled.nav`
+  width: 95%;
+  height: 66px;
+  flex-shrink: 0;
+  background: black;
+  color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 40px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  font-family: 'Inria Serif', sans-serif;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+
+const LogoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  color: white;
+`;
+
+const LogoText = styled.span`
+  font-size: 30px;
+  font-weight: bold;
+`;
+
+const LogoTextYellow = styled(LogoText)`
+  color: yellow;
+`;
+
+const MenuList = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  align-items: center;
+`;
+
+const StyledNavLink = styled(NavLink)`
+  margin: 0 10px;
+  text-decoration: none;
+  color: white;
+  transition: background-color 0.2s, color 0.2s;
+
+  &:hover {
+    background-color: #317873;
+    color: #fff;
+  }
+
+  &.active {
+    color: #317873;
+  }
+`;
 
 export default NavBar;
