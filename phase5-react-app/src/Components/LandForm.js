47c7020ace2a5fa8f84702ef46326623ed944039
Dennismarangu <dennismutumamarangu@gmail.com>
2023-08-05 09:54:17 +0300

    feat: LandForm update

--- phase5-react-app/src/Components/LandForm.js
+++ phase5-react-app/src/Components/LandForm.js
@@ -1,7 +1,28 @@
 import React, { useState } from 'react';
 
 function LandForm() {
+    const [userName, setUserName] = useState('');
+    const [landImage, setLandImage] = useState('');
+    const [landDescription, setLandDescription] = useState('');
+    const [sizeInAcres, setSizeInAcres] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const formData = {
+          user_name: userName,
+          image: landImage,
+          description: landDescription,
+          size_in_acres: sizeInAcres,
+        };
 
 };
+};
 
+
+return (
+    <div>
+        
+    </div>
+)
 export default LandForm;
