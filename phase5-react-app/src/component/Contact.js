286caace2dd4e4a9f9daa68beb8323d46a3c5e22
MPChege <mark.wanyoike@strathmore.edu>
2023-08-07 00:07:05 +0300

    Contact css

--- phase5-react-app/src/component/Contact.js
+++ phase5-react-app/src/component/Contact.js
@@ -1,76 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './ContactUs.css';
 
-const ContactForm = () => {
-  const [formStatus, setFormStatus] = React.useState('Send');
-  const onSubmit = (e) => {
+const ContactUs = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-    setFormStatus('Submitting...');
-    const { name, email, message } = e.target.elements;
-    let conFom = {
-      name: name.value,
-      email: email.value,
-      message: message.value,
-    };
-    console.log(conFom);
+    // Handle form submission logic here
+    // For demonstration purposes, we'll just log the values
+    console.log('Name:', name);
+    console.log('Email:', email);
+    console.log('Message:', message);
   };
 
   return (
-    <div className="container" style={containerStyle}>
-      <div className="background-image" style={backgroundImageStyle} />
-      <div className="contact-form mt-5" style={contactFormStyle}>
-        <h2 className="mb-3">React Contact Form Component Example</h2>
-        <form onSubmit={onSubmit}>
-          <div className="mb-3">
-            <label className="form-label" htmlFor="name">
-              Name
-            </label>
-            <input className="form-control" type="text" id="name" required />
+    <div className="contact-us-container">
+      <div className="contact-us-form">
+        <h2>Contact Us</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
           </div>
-          <div className="mb-3">
-            <label className="form-label" htmlFor="email">
-              Email
-            </label>
-            <input className="form-control" type="email" id="email" required />
+
+          <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
           </div>
-          <div className="mb-3">
-            <label className="form-label" htmlFor="message">
-              Message
-            </label>
-            <textarea className="form-control" id="message" required />
+
+          <div className="form-group">
+            <label htmlFor="message">Message:</label>
+            <textarea
+              id="message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              required
+            />
           </div>
-          <button className="btn btn-danger" type="submit">
-            {formStatus}
-          </button>
+
+          <button type="submit">Send</button>
         </form>
       </div>
     </div>
   );
 };
 
-const containerStyle = {
-  position: 'relative',
-};
-
-const backgroundImageStyle = {
-  position: 'absolute',
-  top: 0,
-  right: 0,
-  bottom: 0,
-  left: 0,
-  backgroundImage:
-    "url('https://images.unsplash.com/photo-1515150144380-bca9f1650ed9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80')",
-  backgroundSize: 'cover',
-  backgroundRepeat: 'no-repeat',
-  backgroundPosition: 'right center',
-  zIndex: -1,
-};
-
-const contactFormStyle = {
-  backgroundColor: 'rgba(255, 255, 255, 0.8)',
-  padding: '20px',
-  maxWidth: '600px',
-  margin: '20px',
-};
-
-export default ContactForm;
-
+export default ContactUs;
