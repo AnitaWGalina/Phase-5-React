513a914cf42ee4b34f16cea78e33edca5bd98898
Alex Njagi <1992anjagi@gmail.com>
2023-08-08 13:05:54 +0000

    feat: Profile page styled

--- phase5-react-app/src/component/Profile.js
+++ phase5-react-app/src/component/Profile.js
@@ -1,13 +1,11 @@
 import React from "react";
+import {Box, Button, Text, Flex, Avatar, Divider} from '@chakra-ui/react';
 import { useAuth } from "../context/AuthContext";
 import { NavLink } from "react-router-dom";
 import DeleteAccount from "./DeleteAccount";
 
 const Profile = () => {
   const { user, logout } = useAuth();
-  // const token = localStorage.getItem('jwt')
-  // const [deleted, setDeleted] = useState(false);
-  // const navigate = useNavigate(); // Add this line to access the navigation function
 
   if (!user) {
     return <p>Please log in to view your profile.</p>;
@@ -19,21 +17,42 @@ const Profile = () => {
   }
 
   return (
-    <div>
-      <h2>Welcome, {user.name}!</h2>
-      <h1>Status: {user.status}</h1>
-      {user.status !== "Public Client" && (
-        <h1>Group Size: {user.group_number}</h1>
-      )}
-      <h1>Email Address: {user.email}</h1>
-      <h1>Phone Number: {user.phone_number}</h1>
-      <h1>Location: {user.location}</h1>
-      <button onClick={handleLogout} >Logout</button>
-      <NavLink to="/update_account">
-        <button>Update My Account</button>
-      </NavLink>
-      <DeleteAccount />
-    </div>
+    <Box p={8} bg="gray.200" h="100vh" display="flex" alignItems="center" justifyContent="center" marginTop="-50px">
+      <Box
+        maxW="600px"
+        p={4}
+        bg="white"
+        borderRadius="md"
+        boxShadow="md"
+        textAlign="center"
+      >
+        <Flex direction="column" alignItems="center" mb={4}>
+          <Avatar size="xl" name={user.name} />
+          <Text mt={2} fontSize="xl" fontWeight="bold">
+            Welcome, {user.name}!
+          </Text>
+        </Flex>
+        <Divider />
+        <Box mt={4} padding="20px">
+          <Text padding="10px">Status: {user.status}</Text>
+          {user.status !== 'Public Client' && (
+            <Text padding="10px">Group Size: {user.group_number}</Text>
+          )}
+          <Text padding="10px">Email Address: {user.email}</Text>
+          <Text padding="10px">Phone Number: {user.phone_number}</Text>
+          <Text>Location: {user.location}</Text>
+        </Box>
+        <Flex justifyContent="space-between" mt={4}>
+          <NavLink to="/update_account">
+            <Button marginRight="50px" colorScheme="teal">Update My Account</Button>
+          </NavLink>
+          <Button colorScheme="red" onClick={handleLogout}>
+            Logout
+          </Button>
+        </Flex>
+        <DeleteAccount />
+      </Box>
+    </Box>
   );
 };
 
